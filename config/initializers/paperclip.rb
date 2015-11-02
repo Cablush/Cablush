2234c0ac9becfa6c2242f5e1436c227ce48fb05d
Oscar Costa <oscarcosta@gmail.com>
2015-11-01 22:50:30 -0300

    Mostrar imagem default do paperclip em produção

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,6 @@
 paperclip_defaults = Rails.application.config_for :paperclip
 paperclip_defaults.symbolize_keys!
 
-Paperclip::Attachment.default_options[:default_url] = "/assets/missing.png"
+Paperclip::Attachment.default_options[:default_url] = ActionController::Base.helpers.asset_path('/assets/missing.png')
 
 Paperclip::Attachment.default_options.merge! paperclip_defaults
