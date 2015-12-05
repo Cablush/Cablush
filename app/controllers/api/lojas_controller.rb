92d9f63757199cc54b961e1d0577b8d7294994a1
Oscar Costa <oscarcosta@gmail.com>
2015-12-05 17:59:52 -0300

    Implementação basica controller de lojas (api)

--- app/controllers/api/lojas_controller.rb
+++ app/controllers/api/lojas_controller.rb
@@ -1,58 +1,83 @@
 class Api::LojasController < Api::ApiController
-	before_action :authenticate_usuario!
+	
+  before_action :authenticate_usuario!
+  
   # GET /lojas
   def index
-    @lojas = Loja.find_like_name(loja_params['nome'])
-    @lojas = @lojas.find_by_estado(loja_params['estado'])
-    @lojas = @lojas.find_by_esporte_id(loja_params['esporte'])
+    lojas = Loja.find_like_name(params['nome'])
+    lojas = lojas.find_by_estado(params['estado'])
+    lojas = lojas.find_by_esporte_id(params['esporte'])
     
-    respond_to do |format|
-      format.json { render json: @lojas, 
-        :except => [:id, :created_at, :updated_at, :responsavel_id, :logo_updated_at],
-        :include => { :locais => {:except => [:id, :created_at, :updated_at, :localizavel_id]}}
+    render json: lojas, 
+      :except => [:id, :created_at, :updated_at, :responsavel_id, :logo_updated_at],
+      :include => {
+        :locais => {:except => [:id, :created_at, :updated_at, :localizavel_id, :localizavel_type]},
+        :esportes => {:except => [:created_at, :updated_at]},
+        :horario => {:except => [:id, :created_at, :updated_at, :funcionamento_id, :funcionamento_type]}
       }
-		end
   end
   
-   # GET /lojas/new(.:format)
-  def new
-    @loja = Loja.new
-    @loja.locais.build
-    @loja.build_horario
-  end
-
   # POST /lojas(.:format)
   def create
-    @loja = Loja.new
-    @loja.nome = loja_params['nome']
-    @loja.estado = loja_params['estado']
-    @loja.esporte = loja_params['esporte']
-    @loja.descricao = loja_params['descricao']
-    @loja.telefone = loja_params['telefone']
-    @loja.email = loja_params['email']
-    @loja.facebook = loja_params['facebook']
-    @loja.website = loja_params['website']
-    @loja.fundo = loja_params['fundo']
-    @loja.uuid = loja_params['uuid']
-    
-    @loja = current_usuario.lojas.build(loja_params['loja'])
-        
-    if @loja.save
-      respond_to do |format|
-        format.json { render json: @loja, 
-        :except => [:id, :created_at, :updated_at, :responsavel_id, :logo_updated_at],
-        :include => { :local => {:except => [:id, :created_at, :updated_at, :localizavel_id]}}
-      }
-      end
+    if current_usuario.admin?
+      loja = Loja.new(loja_params)
     else
-      render 'new'
+      loja = current_usuario.lojas.build(loja_params)
+    end
+    
+    if loja.save
+      render json: loja, 
+        :except => [:id, :created_at, :updated_at, :responsavel_id, :logo_updated_at],
+        :include => {
+          :locais => {:except => [:id, :created_at, :updated_at, :localizavel_id, :localizavel_type]},
+          :esportes => {:except => [:created_at, :updated_at]},
+          :horario => {:except => [:id, :created_at, :updated_at, :funcionamento_id, :funcionamento_type]}
+        }
+    else
+      render json: loja.errors
+    end
+  end
+  
+  # GET /lojas/:id
+  def show
+    loja = Loja.find_by_uuid!(params[:id])
+    render json: loja, 
+      :except => [:id, :created_at, :updated_at, :responsavel_id, :logo_updated_at],
+      :include => {
+        :locais => {:except => [:id, :created_at, :updated_at, :localizavel_id, :localizavel_type]},
+        :esportes => {:except => [:created_at, :updated_at]},
+        :horario => {:except => [:id, :created_at, :updated_at, :funcionamento_id, :funcionamento_type]}
+      }
+  end
+  
+  # PATCH/PUT /lojas/:id
+  def update
+    if current_usuario.admin?
+      loja = Loja.find_by_uuid!(params[:id])
+    else
+      loja = current_usuario.lojas.find_by_uuid!(params[:id])
+    end
+    
+    if loja.update(loja_params)
+      render json: loja, 
+        :except => [:id, :created_at, :updated_at, :responsavel_id, :logo_updated_at],
+        :include => {
+          :locais => {:except => [:id, :created_at, :updated_at, :localizavel_id, :localizavel_type]},
+          :esportes => {:except => [:created_at, :updated_at]},
+          :horario => {:except => [:id, :created_at, :updated_at, :funcionamento_id, :funcionamento_type]}
+        }
+    else
+      render json: loja.errors
     end
   end
 
   private
   
   def loja_params
-    params.permit(:nome, :estado, :esporte, :descricao,:telefone, :email, :facebook, :website, :fundo, :uuid)
+    params.require(:loja).permit(:nome, :telefone, :email, :website, :facebook, :logo, :fundo, :descricao, 
+              esporte_ids: [],
+              locais_attributes: [:id, :latitude, :longitude, :logradouro, :numero, :complemento, :bairro, :cidade, :estado, :cep, :pais], 
+              horario_attributes: [:id, :seg, :ter, :qua, :qui, :sex, :sab, :dom, :inicio, :fim, :detalhes])
   end
   
 end
