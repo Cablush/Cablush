118c0ecf8044cba3fb42cfcfba13ac43aa0f524b
jhonnybat <jhonnybat@gmail.com>
2015-12-06 23:00:58 -0200

    Replicando api lojas

--- app/controllers/api/eventos_controller.rb
+++ app/controllers/api/eventos_controller.rb
@@ -14,10 +14,67 @@ class Api::EventosController < Api::ApiController
 		end
   end
   
+  # POST /eventos(.:format)
+  def create
+    if current_usuario.admin?
+      evento = Evento.new(evento_params)
+    else
+      evento = current_usuario.eventos.build(evento_params)
+    end
+    
+    if evento.save
+      render json: evento, 
+        :except => [:id, :created_at, :updated_at, :responsavel_id, :logo_updated_at],
+        :include => {
+          :locais => {:except => [:id, :created_at, :updated_at, :localizavel_id, :localizavel_type]},
+          :esportes => {:except => [:created_at, :updated_at]},
+          :horario => {:except => [:id, :created_at, :updated_at, :funcionamento_id, :funcionamento_type]}
+        }
+    else
+      render json: evento.errors
+    end
+  end
+  
+  # GET /eventos/:id
+  def show
+    evento = Evento.find_by_uuid!(params[:id])
+    render json: evento, 
+      :except => [:id, :created_at, :updated_at, :responsavel_id, :logo_updated_at],
+      :include => {
+        :locais => {:except => [:id, :created_at, :updated_at, :localizavel_id, :localizavel_type]},
+        :esportes => {:except => [:created_at, :updated_at]},
+        :horario => {:except => [:id, :created_at, :updated_at, :funcionamento_id, :funcionamento_type]}
+      }
+  end
+  
+  # PATCH/PUT /eventos/:id
+  def update
+    if current_usuario.admin?
+      evento = Evento.find_by_uuid!(params[:id])
+    else
+      evento = current_usuario.eventos.find_by_uuid!(params[:id])
+    end
+    
+    if evento.update(evento_params)
+      render json: evento, 
+        :except => [:id, :created_at, :updated_at, :responsavel_id, :logo_updated_at],
+        :include => {
+          :locais => {:except => [:id, :created_at, :updated_at, :localizavel_id, :localizavel_type]},
+          :esportes => {:except => [:created_at, :updated_at]},
+          :horario => {:except => [:id, :created_at, :updated_at, :funcionamento_id, :funcionamento_type]}
+        }
+    else
+      render json: evento.errors
+    end
+  end
+
   private
   
   def evento_params
-    params.permit(:nome, :estado, :esporte)
+    params.require(:evento).permit(:nome, :telefone, :email, :website, :facebook, :logo, :fundo, :descricao, 
+              esporte_ids: [],
+              locais_attributes: [:id, :latitude, :longitude, :logradouro, :numero, :complemento, :bairro, :cidade, :estado, :cep, :pais], 
+              horario_attributes: [:id, :seg, :ter, :qua, :qui, :sex, :sab, :dom, :inicio, :fim, :detalhes])
   end
 
 end
--- app/controllers/api/pistas_controller.rb
+++ app/controllers/api/pistas_controller.rb
@@ -14,35 +14,68 @@ class Api::PistasController < Api::ApiController
     end
   end
 
-   # GET /pistas/new(.:format)
-  def new
-    @pista = Pista.new
-    @pista.build_local
-    @pista.build_horario
-  end
-
   # POST /pistas(.:format)
   def create
-    @usuario = Usuario.find_by_uid(pista_params['uid'])
-    @pista = @usuario.pistas.build(pista_params['pista'])
-    puts @usuario;
-        
-    if @pista.save
-      respond_to do |format|
-        format.json { render json: @pistas, 
-        :except => [:id, :created_at, :updated_at, :responsavel_id, :logo_updated_at],
-        :include => { :local => {:except => [:id, :created_at, :updated_at, :localizavel_id]}}
-      }
-      end
+    if current_usuario.admin?
+      pista = Pista.new(pista_params)
     else
-      render 'new'
+      pista = current_usuario.pistas.build(pista_params)
+    end
+    
+    if pista.save
+      render json: pista, 
+        :except => [:id, :created_at, :updated_at, :responsavel_id, :logo_updated_at],
+        :include => {
+          :locais => {:except => [:id, :created_at, :updated_at, :localizavel_id, :localizavel_type]},
+          :esportes => {:except => [:created_at, :updated_at]},
+          :horario => {:except => [:id, :created_at, :updated_at, :funcionamento_id, :funcionamento_type]}
+        }
+    else
+      render json: pista.errors
+    end
+  end
+  
+  # GET /pistas/:id
+  def show
+    pista = Pista.find_by_uuid!(params[:id])
+    render json: pista, 
+      :except => [:id, :created_at, :updated_at, :responsavel_id, :logo_updated_at],
+      :include => {
+        :locais => {:except => [:id, :created_at, :updated_at, :localizavel_id, :localizavel_type]},
+        :esportes => {:except => [:created_at, :updated_at]},
+        :horario => {:except => [:id, :created_at, :updated_at, :funcionamento_id, :funcionamento_type]}
+      }
+  end
+  
+  # PATCH/PUT /pistas/:id
+  def update
+    if current_usuario.admin?
+      pista = Pista.find_by_uuid!(params[:id])
+    else
+      pista = current_usuario.pistas.find_by_uuid!(params[:id])
+    end
+    
+    if pista.update(pista_params)
+      render json: pista, 
+        :except => [:id, :created_at, :updated_at, :responsavel_id, :logo_updated_at],
+        :include => {
+          :locais => {:except => [:id, :created_at, :updated_at, :localizavel_id, :localizavel_type]},
+          :esportes => {:except => [:created_at, :updated_at]},
+          :horario => {:except => [:id, :created_at, :updated_at, :funcionamento_id, :funcionamento_type]}
+        }
+    else
+      render json: pista.errors
     end
   end
 
   private
   
   def pista_params
-    params.permit(:nome, :estado, :esporte, :descricao,:telefone, :email, :facebook, :website, :fundo, :uuid)
+    params.require(:pista).permit(:nome, :telefone, :email, :website, :facebook, :logo, :fundo, :descricao, 
+              esporte_ids: [],
+              locais_attributes: [:id, :latitude, :longitude, :logradouro, :numero, :complemento, :bairro, :cidade, :estado, :cep, :pais], 
+              horario_attributes: [:id, :seg, :ter, :qua, :qui, :sex, :sab, :dom, :inicio, :fim, :detalhes])
   end
+
 end
 
