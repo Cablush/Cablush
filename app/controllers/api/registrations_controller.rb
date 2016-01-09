dfa562c75458c2b70fdbfdba4557049bf09b5cef
Oscar Costa <oscarcosta@gmail.com>
2016-01-09 17:33:19 -0300

    Corrigido registro de usuário.

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -1,16 +1,32 @@
 class Api::RegistrationsController < DeviseTokenAuth::RegistrationsController
   
-  before_action :configure_permitted_parameters
+  include EsporteAutocompletes
   
-  protected
+  def create
+    super do |resource|
+      if params[:lojista]
+        puts "TRUE"
+        resource.lojista!
+      elsif
+        puts "FALSE"
+        resource.esportista!
+      end
+      resource.save
+    end
+  end
+  
+  def update
+    update_esporte_ids(params)
+    super
+  end
+  
+  def sign_up_params
+    params.permit(:nome, :email, :password, :password_confirmation, :lojista)
+  end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) do 
-      |u| u.permit(:nome, :email, :password, :lojista)
-    end
-    devise_parameter_sanitizer.for(:account_update) do 
-      |u| u.permit(:nome, :email, :current_password, :password, esporte_ids: [])
-    end
+  def account_update_params
+    params.permit(:nome, :email, :current_password, :password, 
+                  :password_confirmation, esporte_ids: [])
   end
   
 end
