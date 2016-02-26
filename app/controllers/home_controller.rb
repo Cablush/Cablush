0f6d90fe42e6012b61cb8a41295911cd395cb682
Oscar Costa <oscarcosta@gmail.com>
2016-02-26 19:40:39 -0300

    Created message to user of type "lojista" adds his store (loja) after login.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -20,6 +20,9 @@ class HomeController < ApplicationController
         @title = "Olá Administrador!<br\> \"Com grandes poderes vêm grandes responsabilidades\" by Stan Lee!".html_safe
       elsif current_usuario.lojista?
         @title = "Olá Lojista!<br\> Cadastre sua loja, pista e eventos aqui, e ajude a divulgar o esporte em sua região!".html_safe
+        if current_usuario.lojas.blank?
+          flash.now[:alert] = 'Olá Lojista, você ainda não cadastrou sua loja, <a href="cadastros/lojas/new">clique aqui</a> para cadastrar, e começar a divulgar a sua marca!'.html_safe
+        end
       else 
         @title = ("Olá " + current_usuario.first_name + "!<br\> Ajude seu esporte, divulgando as lojas, pistas e eventos de sua região!").html_safe
       end
