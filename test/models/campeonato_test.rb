1895b8b7e07399d5938566855680a6e89b8f3ef6
Jonathan <jhonnybat@gmail.com>
2016-06-13 15:05:53 -0300

    Fixing fixtures to run tests, didn't create models to run all tests.

--- test/models/campeonato_test.rb
+++ test/models/campeonato_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class CampeonatoTest < ActiveSupport::TestCase
+
+test "Campeontao validate" do
+	c = Campeonato.new
+	assert c.valid? , "Campeontao nao valido"
+end
   # test "the truth" do
   #   assert true
   # end
