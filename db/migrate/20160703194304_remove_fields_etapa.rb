46213efc340a97eb64b0e4547556857b8632567f
Jonathan <jhonnybat@gmail.com>
2016-07-03 17:34:25 -0300

    Migration to fix model and adjust project

---
+++ db/migrate/20160703194304_remove_fields_etapa.rb
@@ -0,0 +1,7 @@
+class RemoveFieldsEtapa < ActiveRecord::Migration
+  def change
+	remove_column :etapas, :provas_id
+	remove_column :etapas, :qtdProvas
+	remove_column :etapas, :numCompetidoresProva
+  end
+end
