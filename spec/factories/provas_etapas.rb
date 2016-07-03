a96885650ba1c3c0af8f793f8dea0bd4c5b7e503
Jonathan <jhonnybat@gmail.com>
2016-07-03 17:35:25 -0300

    RSpec text [not made]

---
+++ spec/factories/provas_etapas.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :provas_etapa do
+    prova "MyString"
+    references "MyString"
+    etapa "MyString"
+    references "MyString"
+    pontuacao "MyString"
+    decimal "MyString"
+  end
+end
