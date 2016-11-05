cc1275c392527cc64c48050fea96e5af1baab495
Oscar Costa <oscarcosta@gmail.com>
2016-11-05 15:30:43 -0200

    Adjust campeonato controller

--- app/controllers/campeonatos_controller.rb
+++ app/controllers/campeonatos_controller.rb
@@ -1,18 +1,45 @@
 class CampeonatosController < ApplicationController
-  def index
-    @campeonatos = Campeonato.all
-    #@campeonatos = @campeonatos.eventos_by_estado(params[:estado]) if params[:estado].present?
-    #@campeonatos = @campeonatos.eventos_by_esporte_categoria(params[:esporte]) if params[:esporte].present?
+  impressionist actions: [:index]
 
-    @clear = params[:filter] && params[:page].blank? || nil
+  # GET /campeonatos(.:format)
+  def index
+    load_campeonatos
+
     @title = I18n.t 'views.campeonatos.title'
 
     flash.now[:alert] = I18n.t 'views.campeonatos.not_found' if @campeonatos.empty?
 
     respond_to do |format|
-      format.html { @campeonatos }
-      #format.js { @campeonatos }
+      format.html
+      format.js
       format.json { render json: @campeonatos.to_json }
     end
+  end
+
+  # GET /campeonatos/:uuid(.:format)
+  def show
+    load_campeonatos unless request.xhr?
+
+    @campeonato = Campeonato.find_by_uuid!(params[:uuid])
+
+    impressionist(@campeonato)
+
+    @title = @campeonato.nome
+
+    respond_to do |format|
+      format.html
+      format.js
+      format.json { render json: @campeonato.to_json }
+    end
+  end
+
+  private
+
+  def load_campeonatos
+    @locais = Local.campeonatos.paginate(page: params[:page], per_page: 9)
+    @locais = @locais.campeonatos_by_estado(params[:estado]) if params[:estado].present?
+    @locais = @locais.campeonatos_by_esporte_categoria(params[:esporte]) if params[:esporte].present?
+
+    @clear = params[:filter] && params[:page].blank? || nil
   end
 end
